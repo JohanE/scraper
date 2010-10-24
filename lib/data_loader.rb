37f53772d7bf105a7d07f88817617e270b78c0ac
JohanE <johan.a.edlund@gmail.com>
2010-10-24 16:40:53 +0200

    code cleanup

--- lib/data_loader.rb
+++ lib/data_loader.rb
@@ -5,15 +5,14 @@ module Assignment
   class DataLoader
     def self.get_data
       path = File.expand_path(File.dirname(__FILE__) + '/../resources/imac.html')
+      # create a Nokogiri document
       doc = Nokogiri::HTML(File.open(path)) do |config|
         config.noblanks
       end
       
-      # Search for nodes by xpath
-      y = 0
+      # Search for nodes using xpath
       nodes = []
       doc.search('//table[@class=\'listing_thumbs\']/*').each do |a_tag|
-        y = y + 1
         nodes << a_tag
       end
       nodes
